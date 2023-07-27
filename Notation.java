05d13ade68a2a92ba4ba604ce8c739ba6396dda3
kshit <kshit@LAPTOP-UB555J6G>
2023-07-27 18:20:56 +0530

    Third commit

---
+++ Notation.java
@@ -0,0 +1,16 @@
+public class Notation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i = 10;  
+
+		int j = 70;
+		
+		int result = i + j;
+		
+		  
+		 System.out.println("Result: " + result);
+	}
+	
+}
