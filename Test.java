bbf079c13c47dd49671b40ebc075a78507b1736c
kshit <kshit@LAPTOP-UB555J6G>
2023-07-27 17:49:42 +0530

    second commit

---
+++ Test.java
@@ -0,0 +1,12 @@
+public class Test{
+  public static void main(String[] args){
+    int i = 10;
+    int j = 7;
+    i = i + j;
+    j = i - j;
+    i = i - j;
+    System.out.println(i);
+    System.out.println(j);
+    // Expected Result , i = 7  and j = 10    
+   }
+}
