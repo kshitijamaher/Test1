c8d20e05c5dec6438f20e37b500c3f3b348d8908
kshit <kshit@LAPTOP-UB555J6G>
2023-07-27 17:37:39 +0530

    commit

---
+++ SwapValue.java
@@ -0,0 +1,21 @@
+public class SwapValue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  int i = 10;
+
+		  int j = 7;
+		    
+		   int x = 0;
+		    x = i;
+		    i = j;
+		    j = x;
+		    
+		    System.out.println(i);
+
+		    System.out.println(j);
+
+		    // Expected Result , i = 7  and j = 10    
+	}
+
+}
