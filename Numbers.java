e740df3bf0f971eda6216bf45439be77386f02c4
kshitijamaher <46354306+kshitijamaher@users.noreply.github.com>
2023-07-31 12:04:59 +0530

    Update Numbers.java

--- Numbers.java
+++ Numbers.java
@@ -3,25 +3,20 @@ import java.util.Scanner;
 public class Numbers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		Scanner in = new Scanner(System.in);
-		double num;
-		double square_num;
-		double cube_num;
-		double fourth_num;
-		
-		System.out.print("Please enter a Number: ");
-		num = in.nextDouble();
-		
-		square_num = num * num;
-		cube_num = square_num * num;
-		fourth_num = cube_num * num;
-		
-		System.out.printf("\nSquare: %.2f\n", square_num);
-		System.out.printf("Cube: %.2f\n",cube_num);
-		System.out.printf("Fourth Power: %.2f\n", fourth_num);
+			int num, square, cube, power;
+			Scanner input = new Scanner(System.in);
+
+			System.out.print("Please enter a Number: ");
+			num = input.nextInt();
+
+			square = num * num;
+			cube = square * num;
+			power = cube * num;
+
+			System.out.println("\nOutput Is = " + square + " ," + cube + " ," + power + "\n\n");
 
 	}
 
 }
+
